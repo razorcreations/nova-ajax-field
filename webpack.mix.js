070fac67177cb4fcb9d5ca6a3808b329e3f8735e
Gary Garside <gary@garygarside.com>
2024-11-06 13:20:30 +0000

    Linting and composer version update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,16 @@
 let mix = require('laravel-mix')
 
 mix.setPublicPath('dist')
-    .js('resources/js/field.js', 'js')
-    .vue({version: 3})
-    .webpackConfig({
-        externals: {
-            vue: 'Vue',
-        },
-        output: {
-            uniqueName: 'spatie/nova-ajax-field',
-        },
-        resolve: {
-            symlinks: false
-        }
-    })
+	.js('resources/js/field.js', 'js')
+	.vue({version: 3})
+	.webpackConfig({
+		externals: {
+			vue: 'Vue',
+		},
+		output: {
+			uniqueName: 'spatie/nova-ajax-field',
+		},
+		resolve: {
+			symlinks: false
+		}
+	})
